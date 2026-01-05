b8cae1c85fa343fda5dc96159cc4dae386b7d139
y95Weber <Yves_weber@hotmail.ch>
2026-01-05 10:46:17 +0100

    Update echo-client.py

--- Python_Code_Beispiele/Yves/echo-client.py
+++ Python_Code_Beispiele/Yves/echo-client.py
@@ -3,22 +3,22 @@ import socket # Importiert das Modul, um Sockets für Netzwerkkommunikation zu v
 # Hostadresse und Portnummer angeben
 HOST = input("IP des Servers angeben: ")
 PORT = int(input("Portnummer angeben "))
-anzahl = int(input("Wie viele nachrichten sollen gesendet werden? "))
-
+message = 1
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+    while True:
+        # Nachricht an den Server senden
+        sock.sendto(str(message).encode(), (HOST, PORT)) # Sendet Nachricht an angegebene IP und Port
 
-    zahl_client = 1
+        # Antwort vom Server empfangen und Ausgeben
+        data, addr = sock.recvfrom(1024) # Wartet auf Antwort vom Server
+        print(f"Send: {message}, Received: {data.decode()}")
+        message = int(data.decode())
 
-    for i in range (anzahl):
-
-        # Aktueller Werd senden
-        client_message = str(zahl_client).encode()
-        sock.sendto(client_message, (HOST, PORT))
-
-        # Antwort empfangen
-        data, addr = sock.recvfrom(1024)
-        server_wert = int(data.decode())
-
-        print(f"Server antwortet: {server_wert}")
-
-        zahl_client = server_wert + 1
+        # UDP Ping Pong wiederholen ?
+        again = input("Continue? (j/n): ").strip().lower()
+        if again != "j":
+            print("Ping Pong finished!")
+            break
+        # Nächste Zahl vorbereiten
+        else:
+            message += 1
