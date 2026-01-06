b68dceed5bb3d74115494088f3e9c5d8bd37323d
y95Weber <Yves_weber@hotmail.ch>
2026-01-06 09:31:57 +0100

    Update echo-server-mit-Fehlerbehandlung.py

--- Python_Code_Beispiele/Yves/echo-server-mit-Fehlerbehandlung.py
+++ Python_Code_Beispiele/Yves/echo-server-mit-Fehlerbehandlung.py
@@ -2,13 +2,12 @@ import socket
 
 HOST = "0.0.0.0" # Bindet an alle Netzwerkschnittstellen
 PORT = 65432
+last_message = 0
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     sock.bind((HOST, PORT))
     sock.settimeout(50)
     print(f"UDP server listening on {PORT}")
-
-    last_message = None
 
     while True:
         try:
@@ -26,11 +25,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         print(f"Received from Client {addr}: {message}")
 
         # Fehlerprüfung: kommt die erwartete nächste Zahl?
-        if last_message is not None and message != last_message + 1:
+        if message != last_message + 1:
             print("UDP receive is wrong on SERVER!!!")
-
-        last_message = message
-
+        last_message += 2
         # Antwort zurück
         response = str(message + 1).encode()
         sock.sendto(response, addr)
